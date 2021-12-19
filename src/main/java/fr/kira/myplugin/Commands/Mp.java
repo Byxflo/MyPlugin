b6727773fa9e3ae83020b6956f1535a9e846794e
Byxflo <naoneflo33@gmail.com>
2021-12-19 23:15:32 +0100

    Created Mp Command

--- src/main/java/fr/kira/myplugin/Commands/Mp.java
+++ src/main/java/fr/kira/myplugin/Commands/Mp.java
@@ -1,13 +1,32 @@
 package fr.kira.myplugin.Commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import javax.swing.*;
+import java.util.Arrays;
 
 public class Mp implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        return false;
+    public boolean onCommand(CommandSender commandSender, Command command, String alias, String[] args) {
+        if (args.length>=2) {
+            Player player = Bukkit.getPlayer(args[0]);
+            if (player==null) {
+                commandSender.sendMessage("Ce joueur n'est pas connecté, utilise ta matière grise.");
+            }
+            else {
+                String[] messages = Arrays.copyOfRange(args, 1, args.length);
+                String message = String.join(" ", messages);
+                player.sendMessage(message);
+            }
+        }
+        else {
+            commandSender.sendMessage("Met les bon nombres d'arguments, même si tu sais pas ce que c'est ni pourquoi.");
+        }
+        return true;
     }
 }
--- src/main/java/fr/kira/myplugin/Main.java
+++ src/main/java/fr/kira/myplugin/Main.java
@@ -1,6 +1,13 @@
 package fr.kira.myplugin;
 
+import fr.kira.myplugin.Commands.Mp;
+import fr.kira.myplugin.Commands.R;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getCommand("msg").setExecutor(new Mp());
+        getCommand("r").setExecutor(new R());
+    }
 }
